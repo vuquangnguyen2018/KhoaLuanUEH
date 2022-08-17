ea6c876235dc2fcb58d81be8e8b687f6a223e755
Vu Quang Nguyen <vuquangnguyen2018@gmail.com>
2022-08-18 01:19:40 +0700

    update

--- Dataset/Phan tich Thong ke.R
+++ Dataset/Phan tich Thong ke.R
@@ -1,32 +1,35 @@
 library(readxl)
 library(dplyr)
 library(janitor) # GROUP SUMMARY
-library(purrr)
 library(readr) # MA TRAN PEARSON
 library(strucchange) # KIEM DINH CHOW
-
+library(plm) # KIEM DINH HAUSMAN
+library(lmtest) # KIEM DINH Breusch-Pagan Test.
 
 Data <- read_excel("Working/UEH/KhoaLuanUEH/Dataset/Report.xlsx", 
                      sheet = "Output")
+Data<-select(Data,-1)
 
-ViewData<-select(Data,-1)
+#-- FILTERING
+Top_Ticket<-Data %>% 
+  na.omit() %>% # Drop NA 
+  filter(case_when(`Nam`==2021 ~  `Tong Tai san`>= 100*10^3 & `Von hoa thi truong`>=1000 )) %>%# Tong Tai san >= 1000B, Von Hoa >=1000B 
+  select(`Ma CK`)
+
+
+# APPLIED FILTER -> DATASET_1
+Data <- right_join(Data,Top_Ticket, by = "Ma CK")
+Data<-Data %>%
+  na.omit()
+
+
 View(Data)
 
-# THONG KE MO TA --------------------------
-Data %>%
-  select(`Nhom nganh`,SIZE,PROF,LIQD,UNIQ,TANG) %>%
-  split(.$`Nhom nganh`) %>%
-  map(summary)
-
-
-Data %>%
-  select(SIZE,PROF,LIQD,UNIQ,TANG,`GDP (%)`,COVID) %>%
-  summary()
 
 # PEARSON TABULATE CORRELATION
 Corr <- cor(Data %>% 
               na.omit %>% 
-              select(SIZE,PROF,LIQD,UNIQ,TANG,`GDP (%)`,COVID)) # lenh na.omit de loai bo dong na (missing value)
+              select(GRWTH,SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID,INDS)) # lenh na.omit de loai bo dong na (missing value)
 
 upper<-round(Corr,4)
 upper[upper.tri(Corr)]<-""
@@ -34,17 +37,89 @@ upper<-as.data.frame(upper)
 
 View(upper)
 
-
-#MODEL: Short Term STLEV
-
+#--------------
 
 
-# KIEM DINH CHOW TEST
-sctest(Data$STLEV~Data$SIZE+Data$PROF+Data$LIQD+Data$UNIQ+Data$TANG, type = "Chow", point = 10)
+#_-----------
+Report<-Data %>%
+  group_by(`Ma CK`) %>%
+  select(STLEV,LTLEV, BLEV) %>%
+  summarise_at(vars(STLEV,LTLEV, BLEV),              # Specify column
+               list(mean)) 
+
+View(Report)
+
+#--- Report Nhom nganh
+Data %>%
+  filter_at(vars(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID,INDS), all_vars(!is.na(.))) %>% # Loai bo MISSING VALUES
+  group_by(`Nhom nganh`) %>%
+  select(STLEV,LTLEV, BLEV) %>%
+  summarise_at(vars(STLEV,LTLEV, BLEV),              # Specify column
+               list(mean)) 
+#--- Report Nhom nganh
+Data %>%
+  filter_at(vars(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID,INDS), all_vars(!is.na(.))) %>%
+  group_by(`Nhom nganh`) %>%
+  select(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID) %>%
+  summarise_at(vars(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID),              # Specify column
+               list(mean)) 
 
 
 
+# THONG KE MO TA --------------------------
+Data %>%
+  filter_at(vars(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID,INDS), all_vars(!is.na(.))) %>%
+  select(`Nhom nganh`,SIZE,PROF,LIQD,UNIQ,TANG) %>%
+  split(.$`Nhom nganh`) %>%
+  map(summary)
 
+
+Data %>%
+  filter_at(vars(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID,INDS), all_vars(!is.na(.))) %>%
+  select(SIZE,PROF,LIQD,UNIQ,TANG,GDP,COVID) %>%
+  summary()
+
+
+
+#MODEL TEST: Method: OLS, RE, FE
+
+# TESTING MODEL: 
+# Chow: H0: OLS Gop phu hop , H1: FE phu hop -> p_value <5% -> Chon FE
+# Hausman: H0: RE phu hop, H1: FE phu hop -> p_value <5% -> chon FE
+# Breusch-Pagan Test H0: OLS Gop; H1: RE Phu hop -> p_value < 5% -> Chon RE
+
+#--MODEL: Short Term STLEV
+reg_STLEV <- STLEV ~ GRWTH + SIZE + PROF + LIQD + UNIQ + TANG +GDP +COVID 
+#--MODEL: Short Term LTLEV
+reg_LTLEV <- LTLEV ~ GRWTH  + PROF + LIQD + UNIQ + TANG +GDP +COVID 
+#--MODEL: Short Term BLEV
+reg_BLEV <- BLEV ~ GRWTH + PROF + LIQD + UNIQ + TANG +GDP +COVID 
+
+
+
+# KIEM DINH CHOW TEST -> SHORT TERM
+sctest(reg_STLEV, data = Data, type = "Chow", point = 10)
+# KIEM DINH CHOW TEST -> LONG TERM
+sctest(reg_LTLEV, data = Data, type = "Chow", point = 10)
+# KIEM DINH CHOW TEST -> BOOK VALUE OF DEBT
+sctest(reg_BLEV, data = Data, type = "Chow", point = 10)
+
+
+# KIEM DINH HAUSMAN TEST -> SHORT TERM
+phtest(reg_STLEV, data = Data)
+# KIEM DINH HAUSMAN TEST -> LONG TERM
+phtest(reg_LTLEV, data = Data)
+# KIEM DINH HAUSMAN TEST -> BOOK VALUE OF DEBT
+phtest(reg_BLEV, data = Data)
+
+
+
+# KIEM DINH  Breusch-Pagan Test -> SHORT TERM
+bptest(reg_STLEV, data = Data)
+# KIEM DINH Breusch-Pagan Test -> LONG TERM
+bptest(reg_LTLEV, data = Data)
+# KIEM DINH Breusch-Pagan Test -> BOOK VALUE OF DEBT
+bptest(reg_BLEV, data = Data)
 
 
 
